56a5d21046124798b0c297a37171f75b93f45b94
Biswa <barikbiswa143@gmail.com>
2023-03-11 23:15:08 +0530

    Contesxt Api added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../Store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -60,6 +61,8 @@ const Login = (props) => {
       isValid: null,
     }
   );
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -132,7 +135,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(
+    authCtx.onLogin(
       emailState.value,
       passwordState.value,
       collegeNameState.value
